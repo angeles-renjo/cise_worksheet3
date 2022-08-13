88376de1ee669194a62c9b612853dae3a9f6656a
angeles-renjo <82358240+angeles-renjo@users.noreply.github.com>
2022-08-13 23:22:32 +1200

    setting heroku

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config({path: "./cofig.env"});
+const path = require ('path');
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
@@ -16,11 +18,24 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
+
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/my-app/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "my-app", "build", "index.html"));
+    });
+}else {
+    app.get("/", (req, res) => {
+        res.send("Api is running");
+    });
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
