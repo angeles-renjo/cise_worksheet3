72db3ef780caf58b45024963914eb7da86f71ffb
angeles-renjo <82358240+angeles-renjo@users.noreply.github.com>
2022-08-14 00:29:14 +1200

    final deployment

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-require("dotenv").config({path: "./cofig.env"});
-const path = require ('path');
-const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
+require("dotenv").config({ path: './config.env' });
+const path = require("path");
+const express = require("express");
+const connectDB = require("./config/db");
+var cors = require("cors");
 
 // routes
-const books = require('./routes/api/books');
+const books = require("./routes/api/books");
 
 const app = express();
 
@@ -18,24 +18,21 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-
 // use Routes
-app.use('/api/books', books);
+app.use("/api/books", books);
 
-const port = process.env.PORT || 8082;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/my-app/build")));
 
-
-
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/my-app/build")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "my-app", "build", "index.html"));
-    });
-}else {
-    app.get("/", (req, res) => {
-        res.send("Api is running");
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "my-app", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api is run");
+  });
+  console.log(process.env.NODE_ENV);
 }
+const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
